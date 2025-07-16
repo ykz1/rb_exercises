62ba7a303c828f3789b6d6268118af33b2ef640e
Kyle Zhao <kyle.y.zhao@gmail.com>
2025-07-16 15:45:47 -0400

    Accessor exercises

---
+++ rb120_oop/oo_basics_accessor/8.rb
@@ -0,0 +1,15 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name.clone
+  end
+
+end
+
+person1 = Person.new('James')
+person1.name.reverse!
+puts person1.name
