d85456961efb8639cb5dc8a53ee30030d78ead66
Kyle Zhao <kyle.y.zhao@gmail.com>
2025-08-03 14:31:20 -0400

    Some RB120 Med 1 exercises

---
+++ rb120_oop/medium_1/2.rb
@@ -0,0 +1,73 @@
+class FixedArray
+
+  attr_accessor :arr
+
+  def initialize(elements)
+    @arr = [nil] * 5
+  end
+
+  def [](index)
+    raise IndexError.new("Out of index") if [(index + 1), -index].max > arr.size
+    arr[index]
+  end
+
+  def []=(index, value)
+    raise IndexError.new("Out of index") if [(index + 1), -index].max > arr.size
+    arr[index] = value
+  end
+
+  def to_a
+    arr.clone
+  end
+
+  def to_s
+    arr.to_s
+  end
+
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+puts
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
